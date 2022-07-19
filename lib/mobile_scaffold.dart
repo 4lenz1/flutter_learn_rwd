313f074af160bb02a7b6897deaf2f2e3ea0415a4
4lenz1 <pingfallenzone@gmail.com>
2022-07-19 17:06:37 +0900

    add tile

--- lib/mobile_scaffold.dart
+++ lib/mobile_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn_rwd/constants.dart';
+import 'package:flutter_learn_rwd/util/my_box.dart';
+import 'package:flutter_learn_rwd/util/my_tile.dart';
 
 class MobileScaffold extends StatefulWidget {
   MobileScaffold({Key? key}) : super(key: key);
@@ -14,7 +16,33 @@ class _MobileScaffoldState extends State<MobileScaffold> {
     return Scaffold(
       appBar: appBar,
       backgroundColor: defaultBackground,
-      drawer: appDrawer
+      drawer: appDrawer,
+      body: Column(
+        children: [
+          // 4 box
+          AspectRatio(
+            aspectRatio: 1,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return const MyBox();
+                },
+              ),
+            ),
+          ),
+
+          // Tile
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: ((context, index) {
+                    return const MyTile();
+                  }))),
+        ],
+      ),
     );
   }
 }
